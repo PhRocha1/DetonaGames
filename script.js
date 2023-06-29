38a9cbbed2469847ca3768e773f7699257e0bbea
PhRocha1 <pedroextreme2003@gmail.com>
2023-06-29 16:44:38 -0300

    Beta site

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    alert("Compartilhe o nosso site! Deixe seu feedback para melhorar sua experiência!");
+  }
+
+  setInterval(showMessage, 300000); // Exibe a mensagem a cada 1 minuto (60.000 milissegundos)
+
